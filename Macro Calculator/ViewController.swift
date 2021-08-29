37b2b26c0546e8432bcf8c2a3af9b092b47b8a32
bryanhdz <bryhernandez@csumb.edu>
2021-08-28 19:21:09 -0700

    Initial Commit

---
+++ Macro Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Macro Calculator
+//
+//  Created by Bryan Hernandez on 8/28/21.
+//  Copyright © 2021 Bryan Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
